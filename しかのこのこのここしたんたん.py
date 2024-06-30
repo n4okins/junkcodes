a564567af90ac02deb62ae60eeff46ffb68a60e7
n4okins <63445867+n4okins@users.noreply.github.com>
2024-06-30 16:51:33 +0900

    add しかのこのこのここしたんたん

---
+++ しかのこのこのここしたんたん.py
@@ -0,0 +1,15 @@
+import random
+from collections import deque
+
+"""
+しかのこのこのここしたんたん*3が出るまでしかのこのこのここしたんたんする
+"""
+
+q = deque(maxlen=30)
+src = ["しか", "の", "こ", "のこのこ", "こしたんたん"]
+while True:
+    q.append(random.choice(src))
+    text = "".join(q)
+    print(text, end="")
+    if text.endswith("しかのこのこのここしたんたん"):
+        break
